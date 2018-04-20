4c501181cd800aaa392dc42a2d337930ff1b10dc
ryankwteo <ryankt2@illinois.edu>
2018-04-19 21:36:52 -0500

    Added something.

--- app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.illinois.cs.cs125.mp7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openMeals(View view) {
+        Intent intent = new Intent(this, MealActivity.class);
+        startActivity(intent);
     }
 }
