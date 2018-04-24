b5de2e5da3a635d2a095291863dcc74da2077c87
ryankwteo <ryankt2@illinois.edu>
2018-04-23 21:32:18 -0500

    Added stuff that doesn't work.

--- app/src/main/java/edu/illinois/cs/cs125/mp7/CocktailActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/CocktailActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class CocktailActivity extends AppCompatActivity {
 
@@ -11,10 +16,31 @@ public class CocktailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktail);
+
+        Intent intent = getIntent();
+        String json = intent.getStringExtra(SearchActivity.jsonToParse);
+        if (json != null) {
+            String instructions = getInstructions(json);
+            TextView textView = findViewById(R.id.instructionsCocktail);
+            textView.setText(instructions);
+        }
     }
 
     public void openSearch(View view) {
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
+
+    public static String getInstructions(final String json) {
+        if (json == null) {
+            return null;
+        } else {
+            JsonParser parser = new JsonParser();
+            JsonArray drinks = parser.parse(json).getAsJsonArray();
+            JsonObject drink = drinks.get(0).getAsJsonObject();
+
+            return drink.get("strInstructions").getAsString();
+        }
+    }
+
 }
--- app/src/main/java/edu/illinois/cs/cs125/mp7/SearchActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/SearchActivity.java
@@ -1,7 +1,22 @@
 package edu.illinois.cs.cs125.mp7;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class SearchActivity extends AppCompatActivity{
 
@@ -10,4 +25,64 @@ public class SearchActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
+    static String json;
+    static String jsonToParse;
+
+    public void searchByName(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(SearchActivity.this);
+        builder.setTitle("Cocktail Name Search");
+        final EditText input = new EditText(SearchActivity.this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(final DialogInterface dialog, final int unused) {
+
+                String nameToSearch = input.getText().toString().trim().toLowerCase();
+                startAPICall();
+                Intent toCocktailActivity = new Intent(SearchActivity.this,
+                        CocktailActivity.class);
+                toCocktailActivity.putExtra(jsonToParse, json);
+                startActivity(toCocktailActivity);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(final DialogInterface dialog, final int unused) {
+                dialog.cancel();
+            }
+        });
+
+        // Display the dialog
+        builder.show();
+    }
+
+    private static RequestQueue requestQueue;
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            json = response.toString();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
