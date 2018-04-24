01eace2260ee0ca217d0368b91045684930553ad
tony0 <shichen5@illinois.edu>
2018-04-23 19:27:32 -0500

    ???

--- app/src/main/java/edu/illinois/cs/cs125/mp7/CocktailActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/CocktailActivity.java
@@ -2,6 +2,8 @@ package edu.illinois.cs.cs125.mp7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class CocktailActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CocktailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktail);
+    }
+
+    public void openSearch(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
     }
 }
